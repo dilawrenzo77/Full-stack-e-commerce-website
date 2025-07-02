e11b64590764662f1b3acf60b1ecf457c8ba5014
lawrence <mbatalawrence@gmail.com>
2025-07-02 02:09:06 -0700

    another update

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
    env: {
     DATABASE_URL: process.env.DATABASE_URL,
-    PGSSLMODE: process.env.PGSSLMODE || 'require'
+    PGSSLMODE: process.env.NODE_ENV === 'production' ? 'require' : 'disable',
+
+    // Recommended for serverless environments
+    output: process.env.NODE_ENV === 'production' ? 'standalone' : undefined,
   },
   // Other Next.js configuration options can go here
   reactStrictMode: true,
